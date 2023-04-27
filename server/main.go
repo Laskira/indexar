adc7169a1077d31457a85d4aab7f0055951606cf
Laskira <yagoez3@misena.edu.co>
2023-04-26 19:27:28 -0500

    Delete of commits

--- server/main.go
+++ server/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 
+	"github.com/Laskira/indexar/server/directories"
 	"github.com/Laskira/indexar/server/routes"
+	"github.com/Laskira/indexar/server/zincsearch"
 )
 
 func main() {
 
 	//Get directorys and converting file in NJson
-	// directories.GetDirectorysAndConvert()
+	directories.GetDirectorysAndConvert()
 
-	// zincsearch.SendToZincSearch()
+	zincsearch.SendToZincSearch()
 
 	fmt.Println("Succes files indexing")
 
